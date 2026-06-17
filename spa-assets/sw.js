5f2ec84f1d41fa47cc4c4c119506197dbd5efc4a
coreflux-creator <kunalkverma@gmail.com>
2026-06-16 22:03:44 -0400

    Move treasury account screens to v1 API routes

--- spa-assets/sw.js
+++ spa-assets/sw.js
@@ -11,7 +11,7 @@
  * primarily rely on this SW for offline shell + faster repeat loads.
  */
 
-const CACHE_VERSION = 'coreflux-Cta5W-L5';
+const CACHE_VERSION = 'coreflux-BRk0-Up9';
 const APP_SHELL = [
   '/',
   '/spa.php',
