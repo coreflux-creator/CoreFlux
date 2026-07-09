5db3eb8fd760c891c09bc76f33e91faefbb98853
coreflux-creator <kunalkverma@gmail.com>
2026-07-08 21:45:17 -0400

    Build dashboard bundle for JobDiva cleanup

--- spa-assets/sw.js
+++ spa-assets/sw.js
@@ -11,7 +11,7 @@
  * primarily rely on this SW for offline shell + faster repeat loads.
  */
 
-const CACHE_VERSION = 'coreflux-BIiowezQ';
+const CACHE_VERSION = 'coreflux-BteSOcGI';
 const APP_SHELL = [
   '/',
   '/spa.php',
