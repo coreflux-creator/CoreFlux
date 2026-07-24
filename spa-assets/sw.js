3d4a8fac0defba9d9378877779ed67dd77dc1add
coreflux-creator <kunalkverma@gmail.com>
2026-07-23 22:58:53 -0400

    Build field-first mapping bundle

--- spa-assets/sw.js
+++ spa-assets/sw.js
@@ -11,7 +11,7 @@
  * primarily rely on this SW for offline shell + faster repeat loads.
  */
 
-const CACHE_VERSION = 'coreflux-BPdeMucs';
+const CACHE_VERSION = 'coreflux-CMvo6veU';
 const APP_SHELL = [
   '/',
   '/spa.php',
